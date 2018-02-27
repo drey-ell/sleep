ddaf7a95841f81275042394ea160bb7fea48d16d
ttakloyv <dreyelldreyell@hotmail.com>
2018-02-27 14:37:03 -0600

    zzzzzz
    sdfdsf

---
+++ if logic/sketch.js
@@ -0,0 +1,9 @@
+// the setup function will only run once (before the draw loop begins)
+// this is where you want to set up the environment (size of canvas, etc)
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw(){
+  background(200,100,100);
+}
